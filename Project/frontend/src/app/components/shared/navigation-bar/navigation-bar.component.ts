449eca713b91e911ca0d356e03080ba487b3c468
ulianoff <unaxim2000@mail.ru>
2020-02-17 22:02:19 +0300

    create angular project and implement 2 modules

---
+++ Project/frontend/src/app/components/shared/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'e2m-navigation-bar',
+  templateUrl: './navigation-bar.component.html',
+  styleUrls: ['../../../../assets/styles/shared/navigation-bar/navigation-bar.component.less']
+})
+export class NavigationBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Project/frontend/src/app/components/shared/shared.module.ts
@@ -0,0 +1,15 @@
+
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  declarations: [NavigationBarComponent],
+  imports: [],
+  exports: [
+    NavigationBarComponent
+  ],
+  providers: []
+})
+export class SharedModule {
+
+}
