a30aa1821509f8585a4a1f03ae61d0c5a093415d
Ulianoff <umaxim2000@mail.ru>
2020-04-27 21:35:03 +0300

    implement dashboards layout

--- Project/frontend/src/app/components/pages/dashboards/dashboards.component.ts
+++ Project/frontend/src/app/components/pages/dashboards/dashboards.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'e2m-dashboards',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  public navigateToSearch(): void {
+    this.router.navigate(['search']);
   }
 
 }
