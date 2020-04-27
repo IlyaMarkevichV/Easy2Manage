f28f13b1513dd257590fe215e2dbcea9e1c3f450
Ulianoff <umaxim2000@mail.ru>
2020-04-27 17:42:15 +0300

    add filter and dashboards components

---
+++ Project/frontend/src/app/components/pages/dashboards/dashboards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'e2m-dashboards',
+  templateUrl: './dashboards.component.html',
+  styleUrls: ['../../../../assets/styles/pages/dashboards/dashboards.component.less']
+})
+export class DashboardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
